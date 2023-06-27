5970acbfd859697119b3c9dbc6e45f7f9a7bd4b3
thaiphuongnam040499 <thainamvinhuni@gmail.com>
2023-06-27 14:23:51 +0700

    update

---
+++ src/main/java/rikkei/academy/model/Blog.java
@@ -0,0 +1,24 @@
+package rikkei.academy.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nameBlog;
+    private String text;
+    private boolean status;
+}
